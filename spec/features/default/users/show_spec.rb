12878cbd3c0f5cb431a1ed5c216d885d19f7eae3
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 15:20:56 -0600

    Add EditAddressRoute nested inside Default users, fix path helpers for spec and HTML

--- spec/features/default/users/show_spec.rb
+++ spec/features/default/users/show_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe 'User show page', type: :feature do
       it 'I see a link to edit my Home address' do
         visit profile_path
 
-        expect(page).to have_link(@user.addresses.first.nickname, href: address_edit_path(@user.addresses.first))
+        expect(page).to have_link(@user.addresses.first.nickname, href: edit_address_path(@user.addresses.first))
       end
 
       describe 'And I have orders placed in the system' do
