345e9b7b156ca9c743b576746859cac55db2d1f0
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 15:46:07 -0600

    Refactor Address Edit spec to follow Edit Form convention

--- spec/features/addresses/edit_spec.rb
+++ spec/features/addresses/edit_spec.rb
@@ -4,18 +4,21 @@ RSpec.describe 'As a Default user', type: :feature do
   describe 'When I visit an Address edit page' do
     before :each do
       @user = User.create!(email: "test@test.com", password_digest: "t3s7", role: 0, active: true, name: "Testy McTesterson")
-      @address = @user.addresses << create(:address, address: "123 Test St", city: "Testville", state: "Test", zip: "01234", user: @user)
+      @user.addresses << create(:address, address: "123 Test St", city: "Testville", state: "Test", zip: "01234", user: @user)
+      @address = @user.addresses.first
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
     it 'should have a form to edit' do
       visit edit_address_path(@address)
-      
+
+      expect(page).to have_field("Nickname", with: "Home")
       expect(page).to have_field("Address", with: "123 Test St")
       expect(page).to have_field("City", with: "Testville")
       expect(page).to have_field("State", with: "Test")
       expect(page).to have_field("Zip", with: "01234")
+      expect(page).to have_button "Edit Address"
     end
   end
 end
