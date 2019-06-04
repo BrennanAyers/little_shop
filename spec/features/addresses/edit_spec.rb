c5a29abc6442b1b6b8fe685ebf06113c933bc648
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 15:56:37 -0600

    Add initial Update spec for Addresses Edit spec

--- spec/features/addresses/edit_spec.rb
+++ spec/features/addresses/edit_spec.rb
@@ -20,5 +20,27 @@ RSpec.describe 'As a Default user', type: :feature do
       expect(page).to have_field("Zip", with: "01234")
       expect(page).to have_button "Edit Address"
     end
+
+    it 'should allow me to edit my address' do
+      visit edit_address_path(@address)
+
+      fill_in "Nickname", with: "Away"
+      fill_in "Address", with: "666 Fire Rd"
+      fill_in "City", with: "Newark"
+      fill_in "State", with: "Joisey"
+      fill_in "Zip", with: "96669"
+
+      click_button "Edit Address"
+
+      expect(current_path).to eq(profile_path)
+
+      within "#address-#{@address.id}" do
+        expect(page).to have_content("Address Nickname: Away")
+        expect(page).to have_content("Address: 666 Fire Rd")
+        expect(page).to have_content("City: Newark")
+        expect(page).to have_content("State: Joisey")
+        expect(page).to have_content("Zip Code: 96669")
+      end
+    end
   end
 end
