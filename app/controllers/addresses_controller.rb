6221724be4cb481c5a84191c666671d126d099ed
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 16:03:03 -0600

    Add #update with address_params to AddressesController

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -2,4 +2,20 @@ class AddressesController < ApplicationController
   def edit
     @address = Address.find(params[:id])
   end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "Address #{@address.nickname} has been updated!"
+      redirect_to profile_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def address_params
+      params.require(:address).permit(:nickname, :address, :city, :state, :zip)
+  end
 end
