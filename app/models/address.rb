9bc33dc6be9b1558856f36fe3cf39bca8996a54a
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 15:05:50 -0600

    Add before_save callback for addresses to set the newly created user Address to be set to Home

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,4 +2,12 @@ class Address < ApplicationRecord
   belongs_to :user
 
   validates_presence_of :address, :city, :state, :zip
+
+  before_save :set_nickname
+
+  private
+
+  def set_nickname
+    write_attribute(:nickname, 'Home') if self.nickname.nil?
+  end
 end
