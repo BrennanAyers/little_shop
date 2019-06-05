46b6e70a6a98c01e7ac35d95b670591fab0d824d
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-06-04 18:40:02 -0600

    Remove User FK from Orders, Add Address FK to Orders, update Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190604194101) do
+ActiveRecord::Schema.define(version: 20190605002524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,11 +53,11 @@ ActiveRecord::Schema.define(version: 20190604194101) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.bigint "user_id"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.bigint "address_id"
+    t.index ["address_id"], name: "index_orders_on_address_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -74,5 +74,5 @@ ActiveRecord::Schema.define(version: 20190604194101) do
   add_foreign_key "items", "users"
   add_foreign_key "order_items", "items"
   add_foreign_key "order_items", "orders"
-  add_foreign_key "orders", "users"
+  add_foreign_key "orders", "addresses"
 end
